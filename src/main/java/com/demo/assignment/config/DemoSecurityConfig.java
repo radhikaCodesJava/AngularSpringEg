82eebe733da52095d16b1ba2748e05ac74dd152e
radhika inguva <radhika.inguva@gmail.com>
2022-03-09 22:39:38 -0800

    implemented swagger

--- src/main/java/com/demo/assignment/config/DemoSecurityConfig.java
+++ src/main/java/com/demo/assignment/config/DemoSecurityConfig.java
@@ -1,11 +1,20 @@
 package com.demo.assignment.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -23,5 +32,26 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
         	.httpBasic();
 	}
 
+	//http://localhost:8080/swagger-ui.html
+	@Bean
+	public Docket LMSProgramBatchApi() {
+		return  new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				//.apis(RequestHandlerSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("com.demo.assignment"))
+                .paths(PathSelectors.any())
+                //give request mapping path here -check in controller , now for regex input i have given eg: /program.*
+				//.paths(regex("/program.*"))
+	            .build()
+	            //in our case we dont have path mapping so commented
+	            //.pathMapping("/api")
+	           
+	            ;
+			}
 
+	//@Override
+	/*public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}*/
 }
